9a6686840b52694ab8250e4cf404fb28acfc96a1
Nikola Nedeljkovic <nikolanedeljkovic.official@gmail.com>
2025-04-28 02:34:24 +0200

    Added license to main file

--- bootloader/main.asm
+++ bootloader/main.asm
@@ -1,3 +1,10 @@
+; main.asm - simpleOS
+; Copyright (C) 2025 Nikola Nedeljkovic
+; This program is free software: you can redistribute it and/or modify it under the terms of
+; the GNU General Public License, version 3, or (at your option) any later version.
+; You can freely share and/or modify this program under the terms of GPL-3.
+; See the full license text at: https://www.gnu.org/licenses/gpl-3.0.html
+
 org 0x7C00
 bits 16
 
